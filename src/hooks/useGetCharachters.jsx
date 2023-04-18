11fc6afe65632a976c752c0a5b21450796f42141
Christian Alvarenga <>
2023-04-18 10:13:07 -0400

    project finished

--- src/hooks/useGetCharachters.jsx
+++ src/hooks/useGetCharachters.jsx
@@ -1,14 +1,16 @@
 import { useState, useEffect } from "react";
+import { Alert } from "antd";
 
 const useGetCharacters = () => {
     const [characters, setCharacters] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
     const [errorHandler, setErrorHandler] = useState({
         hasError: false,
         errorResponse: {},
     });
 
     useEffect(() => {
-        fetch("https://rickandmortyapi.com/api/character")
+        fetch("https://rickandmortyapi.com/api/character/?page=1")
             .then((response) => response.json())
             .then((data) => setCharacters(data))
             .catch((error) =>
@@ -16,9 +18,30 @@ const useGetCharacters = () => {
             );
     }, []);
 
+    useEffect(() => {
+        changePage();
+    }, [currentPage]);
+
+    useEffect(() => {
+        if (errorHandler.hasError) {
+            <Alert message="Error Text" type="error" />;
+        }
+    }, [errorHandler.hasError]);
+
+    const changePage = () => {
+        fetch(`https://rickandmortyapi.com/api/character/?page=${currentPage}`)
+            .then((response) => response.json())
+            .then((data) => setCharacters(data))
+            .catch((error) =>
+                setErrorHandler({ hasError: true, errorResponse: error })
+            );
+    };
+
     return {
         characters,
         errorHandler,
+        currentPage,
+        setCurrentPage,
     };
 };
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
+import { Pagination } from "antd";
 import CharacterCard from "../../components/CharacterCard";
 import useGetCharacters from "../../hooks/useGetCharachters";
 import styles from "./index.module.css";
 
 const Home = () => {
-    const { characters } = useGetCharacters();
+    const { characters, setCurrentPage, currentPage } = useGetCharacters();
 
     return (
-        <div className={styles.cardsContainer}>
-            {characters?.results?.map((character) => (
-                <CharacterCard character={character} />
-            ))}
+        <div>
+            <div className={styles.cardsContainer}>
+                {characters?.results?.map((character) => (
+                    <CharacterCard character={character} />
+                ))}
+            </div>
+            <div className={styles.paginationContainer}>
+                <Pagination
+                    total={characters?.info?.count}
+                    current={currentPage}
+                    onChange={setCurrentPage}
+                />
+            </div>
         </div>
     );
 };
